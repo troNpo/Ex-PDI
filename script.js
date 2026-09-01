9fcec3cb722e7c0eff97c24be9d86677ec55a401
troNpo Orux Sapiens <97335264+troNpo@users.noreply.github.com>
2026-09-01 20:26:09 +0200

    Remove base map sources from script.js
    Removed base map sources for OpenStreetMap and OpenTopoMap.

--- script.js
+++ script.js
@@ -8,16 +8,6 @@ const baseMaps = {
   'topo': {
     tiles: ['https://a.tile.opentopomap.org/{z}/{x}/{y}.png'],
     maxzoom: 17,
-// Fuentes de mapas base disponibles
-const baseMaps = {
-  'osm': {
-    tiles: ['https://tile.openstreetmap.org/{z}/{x}/{y}.png'],
-    maxzoom: 19,
-    attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-  },
-  'topo': {
-    tiles: ['https://a.tile.opentopomap.org/{z}/{x}/{y}.png'],
-    maxzoom: 17,
     attribution: '&copy; <a href="https://opentopomap.org">OpenTopoMap</a> (CC-BY-SA)'
   },
   'esri': {
