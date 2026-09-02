becda2988918242c8135ec8b67400943cd64e0ae
troNpo Orux Sapiens <97335264+troNpo@users.noreply.github.com>
2026-09-02 05:08:39 +0200

    Implement service worker registration
    Add service worker registration to enhance offline capabilities.

--- script.js
+++ script.js
@@ -897,3 +897,12 @@ document.addEventListener('DOMContentLoaded', () => {
 
  
 });
+//serviceWorker
+if ('serviceWorker' in navigator) {
+  window.addEventListener('load', () => {
+    navigator.serviceWorker.register('/sw.js')
+      .then((reg) => console.log('Service Worker registrado con éxito:', reg.scope))
+      .catch((err) => console.log('Error al registrar el Service Worker:', err));
+  });
+}
+
